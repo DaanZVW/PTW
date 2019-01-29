bbf84ef367735255e24e976b5ccc6de585529070
markvk14 <34057780+markvk14@users.noreply.github.com>
2019-01-29 17:21:06 +0100

    Add files via upload

---
+++ icontainer-master/webpage/iContainer/index.php
@@ -0,0 +1,20 @@
+<?php
+    require "hoofdpagina.php";
+?>
+
+    <main>
+        <div class="wrapper-main">
+            <section class="section-default">
+        <?php
+        if (isset($_SESSION['userId'])){
+            echo '<p>Your are logged in!</p>';
+            header("Location: ../newhome.php");
+        }
+        else{
+            echo '<p>Your are logged out!</p>';
+        }
+        ?>
+            </section>
+        </div>
+
+    </main>
